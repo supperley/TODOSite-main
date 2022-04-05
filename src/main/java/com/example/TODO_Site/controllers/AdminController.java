61c54b508c1239f489b372f341f99b415702dea2
Ladizzz <v.bobrovko@yandex.by>
2022-04-05 19:23:18 +0300

    Privilege escalation fixes

--- src/main/java/com/example/TODO_Site/controllers/AdminController.java
+++ src/main/java/com/example/TODO_Site/controllers/AdminController.java
@@ -37,7 +37,6 @@ public class AdminController {
     @GetMapping("/admin/user/edit/{user}")
     public String userEdit(@PathVariable("user") User user, Model model, Principal principal) {
         model.addAttribute("user", user);
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
         model.addAttribute("roles", Role.values());
         return "user-edit";
     }
