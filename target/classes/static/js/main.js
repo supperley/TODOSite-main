adab6f539d90356ef699b9308a7c2e0f4d847c9b
Ladizzz <v.bobrovko@yandex.by>
2022-05-14 17:27:13 +0300

    Full support of Dark theme

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -1,1 +1,30 @@
 console.log("test");
+
+const checkbox = document.querySelector('.form-check-input');
+const text = document.querySelector('.form-check-label');
+
+// возвращает куки с указанным name,
+// или undefined, если ничего не найдено
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+if (getCookie('theme') === 'dark') {
+    checkbox.checked = true;
+    text.outerHTML = "Темная";
+}
+
+checkbox.addEventListener('change', function () {
+    setTimeout(
+        () => {
+            if (this.checked) {
+                location.href = "?theme=dark";
+            } else {
+                location.href = "?theme=light";
+            }
+        }
+    )
+})
